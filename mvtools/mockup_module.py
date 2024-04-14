b40fa2614baf1d910e757f82a5d033675ded60f7
Wiktor Morawski <wrmorawski@gmail.com>
2024-04-14 18:28:04 +0200

    initial package structure

---
+++ mvtools/mockup_module.py
@@ -0,0 +1,5 @@
+"""This is a mockup module."""
+
+def mockup_function(a: int, b:float) -> float: 
+    "This mockup function divides a by b."
+    return a/b
---
+++ tests/test_mockup_module.py
@@ -0,0 +1,15 @@
+import pytest 
+
+from mvtools.mockup_module import mockup_function
+
+@pytest.fixture
+def a():
+    return 10
+
+@pytest.fixture
+def b(): 
+    return 2.5
+
+def test_mockup_function(a, b): 
+    result = mockup_function(a, b)
+    assert result == 4.0
